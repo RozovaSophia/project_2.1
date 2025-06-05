9f3f31d40618924a0d1da4354abfb363568a0e48
Розова София <rozowasof@yandex.ru>
2025-06-05 22:07:35 +0300

    version with modifications from the teacher

--- src/masks.py
+++ src/masks.py
@@ -1,5 +1,5 @@
 def get_mask_card_number(card_number: str) -> str:
-    # функция, которая принимает номер карты, и возвращает ее маску
+    """функция, которая принимает номер карты, и возвращает ее маску"""
     formatted_number = card_number[:4] + " " + card_number[6:8] + "** **** " + card_number[-4:]
     return formatted_number
 
@@ -9,7 +9,7 @@ print(result)
 
 
 def get_mask_account(account: str) -> str:
-    # функция, которая принимает номер счета, и возвращает его маску
+    """функция, которая принимает номер счета, и возвращает его маску"""
     mask_account = account.replace(account[0:16], "**")
     return mask_account
 
