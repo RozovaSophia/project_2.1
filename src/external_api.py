47a471e1af23cece04a732fc8fe8b612f55d86bc
Розова София <rozowasof@yandex.ru>
2025-06-22 11:53:26 +0300

    added the .env file

--- src/external_api.py
+++ src/external_api.py
@@ -1,17 +1,23 @@
+import os
+from dotenv import load_dotenv
 import requests
 from src.utils import get_fin_transactions
 
+load_dotenv()
+
+api_key = os.getenv('API_KEY')
+
 headers= {
-  "apikey": "A2L53ElgBES9MUePDb46QBpaw10bkvzz"
+    "apikey": f"{api_key}"
 }
 
-def return_amount(data) -> float:
+def return_amount(data):
     for transaction in data:
-        if transaction['operationAmount']['currency']['code'] != "RUB":
-            from_ = transaction['operationAmount']['currency']['code']
-            amount = transaction['operationAmount']['amount']
-            response = requests.get(f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={from_}&amount={amount}", headers=headers)
-            yield response.json()
+            if transaction['operationAmount']['currency']['code'] != "RUB":
+                from_ = transaction['operationAmount']['currency']['code']
+                amount = transaction['operationAmount']['amount']
+                response = requests.get(f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={from_}&amount={amount}", headers=headers)
+                yield response.json()
 
 if __name__ == '__main__':
     result = return_amount(data=get_fin_transactions())
