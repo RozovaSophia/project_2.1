70736e6fea53537f7e6f4be3a7e3643dcc90bf78
Розова София <rozowasof@yandex.ru>
2025-07-03 16:44:40 +0300

    distributed logging throughout the masks module, removed unnecessary logging, and now the function does not accept lists

--- src/masks.py
+++ src/masks.py
@@ -1,6 +1,5 @@
 import logging
 import os
-import typing
 
 app_logger = logging.getLogger(__name__)
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -12,57 +11,50 @@ file_handler.setFormatter(file_formatter)
 app_logger.addHandler(file_handler)
 app_logger.setLevel(logging.DEBUG)
 
-app_logger.info("Подключение к базе данных установлено")
-app_logger.warning("Используется устаревшая функция get_data()")
-app_logger.error("Ошибка при обработке файла")
-app_logger.critical("Повреждение данных обнаружено! Завершение работы...")
+app_logger.info("Программа запущена")
 
-
-def get_mask_card_number(card_number: typing.Any) -> str:
+def get_mask_card_number(card_number: str) -> str:
     """функция, которая принимает номер карты, и возвращает ее маску"""
-    if isinstance(card_number, str):
+    try:
+        if not isinstance(card_number, str):
+            raise TypeError("Ожидается строка, а получен другой тип данных")
         if card_number.isdigit() and len(card_number) == 16:
             formatted_number = card_number[:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
+            app_logger.debug("Введенный номер карты (ЗАМАСКИРОВАННЫЙ): %s", formatted_number)
+            app_logger.info("Функция выполнена успешно")
             return formatted_number
         else:
-            return "Error: invalid number format"
-    else:
-        # return "Please, enter your card number"
-        filter_list = []
-        for item in card_number:
-            if isinstance(item, str):
-                if item.isdigit() and len(item) == 16:
-                    formatted_number = item[:4] + " " + item[4:6] + "** **** " + item[-4:]
-                    filter_list.append(formatted_number)
-        return " ".join(filter_list)
+            app_logger.error("Ошибка: неправильный формат ввода")
+            return None
+    except TypeError as e:
+        logging.error(f"Ошибка типа данных: {e}")
+        return None
 
 
 if __name__ == "__main__":
     result = get_mask_card_number(input("Enter your number card: "))
     print(result)
 
-    app_logger.debug("Введенный номер карты (ЗАМАСКИРОВАННЫЙ): %s", result)
 
-
-def get_mask_account(account: typing.Any) -> str:
+def get_mask_account(account: str) -> str:
     """функция, которая принимает номер счета, и возвращает его маску"""
-    if isinstance(account, str):
+    try:
+        if not isinstance(account, str):
+            raise TypeError("Ожидается строка, а получен другой тип данных")
         if account.isdigit() and len(account) == 20:
             mask_account = account.replace(account[0:16], "**")
+            app_logger.debug("Введенный счет карты (ЗАМАСКИРОВАННЫЙ): %s", mask_account)
+            app_logger.info("Функция выполнена успешно")
             return mask_account
         else:
-            return "Error: invalid account format"
-    else:
-        # return "Please, enter your account number"
-        filter_list = []
-        for item in account:
-            if isinstance(item, str):
-                if item.isdigit() and len(item) == 20:
-                    mask_account = item.replace(item[0:16], "**")
-                    filter_list.append(mask_account)
-        return " ".join(filter_list)
+            app_logger.error("Ошибка: неправильный формат ввода")
+            return None
+    except TypeError as e:
+        logging.error(f"Ошибка типа данных: {e}")
+        return None
 
 
 if __name__ == "__main__":
     result = get_mask_account(input("Enter account: "))
     print(result)
+
