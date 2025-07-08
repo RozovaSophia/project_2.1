4be0f57e21de292f7512046ee771e8819682f28e
Розова София <rozowasof@yandex.ru>
2025-07-08 19:38:31 +0300

    have adjusted the transactions and external_api modules

--- src/transactions.py
+++ src/transactions.py
@@ -5,31 +5,18 @@ import pandas as pd
 
 def reads_financial_transactions(file_path):
     """считывает файл csv, выводит в виде списка словарей"""
-    try:
-        with open(file_path, "r", encoding="UTF-8") as file:
-            reader = csv.DictReader(file)
-            transactions = list(reader)
-            return transactions
-    except FileNotFoundError:
-        print("Ошибка: Файл 'transactions.csv' не найден.")
-        return None
-    except Exception as e:
-        print(f"Произошла ошибка при чтении CSV: {e}")
-        return None
+    with open(file_path, "r", encoding="UTF-8") as file:
+        reader = csv.DictReader(file)
+        transactions = list(reader)
+        return transactions
 
 
 def reads_financial_transactions_excel(file_path):
     """считывает файл excel, выводит в виде списка словарей"""
-    try:
-        excel_data = pd.read_excel(file_path, parse_dates=["date"])
-        transactions = excel_data.to_dict(orient="records")
-        return transactions
-    except FileNotFoundError:
-        print("Ошибка: Файл 'transactions_excel.xlsx' не найден.")
-        return None
-    except Exception as e:
-        print(f"Произошла ошибка при чтении XLSX: {e}")
-        return None
+    excel_data = pd.read_excel(file_path, parse_dates=["date"])
+    transactions = excel_data.to_dict(orient="records")
+    return transactions
+
 
 
 if __name__ == "__main__":
