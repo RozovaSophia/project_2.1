9023385c2d0566e46cbf1d05fca67ce1c816c57f
Розова София <rozowasof@yandex.ru>
2025-07-22 11:54:00 +0300

    added a function that shows the number of descriptions entered by the user

--- src/filtering_transactions.py
+++ src/filtering_transactions.py
@@ -1,8 +1,10 @@
 import re
+from collections import Counter
 
 from src.utils import get_fin_transactions
 
 def process_bank_search(data:list[dict], search:str)->list[dict]:
+    """возвращает совпадения описания транзакции и поисковой строки"""
     matches = []
     for transaction in data:
         if transaction:
@@ -14,4 +16,21 @@ def process_bank_search(data:list[dict], search:str)->list[dict]:
 if __name__ == "__main__":
     transactions = get_fin_transactions()
     result = process_bank_search(data=transactions, search=input())
+    print(result)
+
+
+def process_bank_operations(data:list[dict], categories:list)->dict:
+    """возвращает количество введенных описаний из списка словарей файла json"""
+    descriptions = []
+    for transaction in data:
+        if transaction:
+            if transaction['description'] in categories:
+                descriptions.append(transaction['description'])
+    counted = Counter(descriptions)
+    return dict(counted)
+
+
+if __name__ == "__main__":
+    transactions = get_fin_transactions()
+    result = process_bank_operations(data=transactions, categories=input())
     print(result)
