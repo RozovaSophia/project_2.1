02ccd2e089f537af1eb703b368472605e3b072ee
Розова София <rozowasof@yandex.ru>
2025-07-23 10:00:06 +0300

    I fixed the code that sends the API request and improved the functionality

--- src/external_api.py
+++ src/external_api.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 import requests
@@ -12,38 +13,26 @@ api_key = os.getenv("API_KEY")
 headers = {"apikey": f"{api_key}"}
 
 
-def return_amount(transaction):
-    """Принимает файл с данными о транзакциях и выводит сумму в рублях, если валюта не равна RUB, то конвертирует ее
-    через сторонний сервис"""
-    code = transaction["operationAmount"]["currency"]["code"]
-    amount = transaction["operationAmount"]["amount"]
-
-    if code != "RUB":
-        from_ = code
-        response = requests.get(
-            f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={from_}&amount={amount}",
-            headers=headers,
-        )
-        if response.status_code == 200:
-            result = response.json()
-            if "result" in result:
-                converted_amount = result["result"]
-                return converted_amount
-            else:
-                print("Ошибка: Ключ 'result' отсутствует в ответе API.")
-                return None
-        else:
-            print(f"Ошибка API: Код состояния {response.status_code}, Ответ: {response.text}")
-    else:
-        return amount
+def return_transaction(transactions):
+    converted_transactions = []
+    for transaction in transactions:
+        if transaction:
+            if transaction["operationAmount"]["currency"]["code"] == "RUB":
+                converted_transactions.append(transaction)
+            elif transaction["operationAmount"]["currency"]["code"] == "USD":
+                have = transaction["operationAmount"]["currency"]["code"]
+                amount = transaction["operationAmount"]["amount"]
+                url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={have}&amount={amount}"
+                response = requests.get(url, headers=headers)
+                data = json.loads(response.text)
+                transaction["operationAmount"]["currency"]["code"] = "RUB"
+                transaction["operationAmount"]["currency"]["name"] = "руб."
+                transaction["operationAmount"]["amount"] = round(float(data["result"]), 2)
+                converted_transactions.append(transaction)
+    return converted_transactions
 
 
 if __name__ == "__main__":
     transactions = get_fin_transactions()
-
-    if transactions:
-        for transaction in transactions:
-            result = return_amount(transaction)
-            print(f"Сумма в рублях для транзакции: {result}")
-    else:
-        print("Нет данных о транзакциях для обработки.")
+    result = return_transaction(transactions)
+    print(result)
