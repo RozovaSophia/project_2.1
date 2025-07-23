c81f0165fc6f76e2706dd0e418b8040a8d682e99
Розова София <rozowasof@yandex.ru>
2025-07-23 18:15:45 +0300

    I added a function that writes a csv file to the data folder

--- src/transactions.py
+++ src/transactions.py
@@ -3,11 +3,16 @@ import csv
 import pandas as pd
 
 
-def reads_financial_transactions(file_path):
-    """считывает файл csv, выводит в виде списка словарей"""
-    with open(file_path, "r", encoding="UTF-8") as file:
-        reader = csv.DictReader(file, delimiter=";")
-        return list(reader)
+def reads_financial_transactions(file_path, output_file_path):
+    """считывает файл csv, выводит в виде списка словарей, записывает в новый файл"""
+    with open(file_path, "r", encoding="UTF-8") as infile: # Открываем для чтения
+        reader = csv.DictReader(infile, delimiter=";")
+        data = list(reader)
+        with open(output_file_path, "w", encoding="UTF-8", newline="") as outfile:
+                writer = csv.DictWriter(outfile, fieldnames=data[0].keys(), delimiter=";")
+                writer.writeheader()
+                writer.writerows(data)
+        return data
 
 
 def reads_financial_transactions_excel(file_path):
@@ -18,6 +23,6 @@ def reads_financial_transactions_excel(file_path):
 
 
 if __name__ == "__main__":
-    result_1 = reads_financial_transactions("C:/Users/Thunderobot/Downloads/transactions.csv")
-    result_2 = reads_financial_transactions_excel("C:/Users/Thunderobot/Downloads/transactions_excel.xlsx")
-    print(result_1, result_2)
+    result_1 = reads_financial_transactions("C:/Users/Thunderobot/Downloads/transactions.csv", "../data/operations.csv")
+    #result_2 = reads_financial_transactions_excel("C:/Users/Thunderobot/Downloads/transactions_excel.xlsx")
+    print(result_1)
